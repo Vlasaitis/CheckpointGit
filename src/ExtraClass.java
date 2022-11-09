9e8044b3dac0c7ed76dd7ce5d8f5e429fef74cc1
Vytis Lasaitis <v.lasaitis@gmail.com>
2022-11-09 13:10:00 +0100

    Initial commit

---
+++ src/ExtraClass.java
@@ -0,0 +1,11 @@
+public class ExtraClass {
+    private String name;
+
+    public ExtraClass(String name) {
+        this.name = name;
+    }
+
+    public void printName() {
+        System.out.println("Name: " + this.name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Some text");
+        ExtraClass extra = new ExtraClass("Vytis");
+        extra.printName();
+    }
+}
